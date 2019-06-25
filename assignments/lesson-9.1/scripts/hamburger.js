9775812d65231473e791c209b082e1874b7be581
ninjawriter222 <devin.a.poe@gmail.com>
2019-06-25 09:48:56 -0700

    Add files via upload

---
+++ assignments/lesson-9.1/scripts/hamburger.js
@@ -0,0 +1,16 @@
+function toggleHam() {
+    document.getElementByClassName("hamburger")[0].classList.toggle("responsive");
+  }
+
+window.onclick = function(event) {
+    if (!event.target.matches('.menu')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('responsive')) {
+          openDropdown.classList.remove('responsive');
+        }
+      }
+    }
+  }
