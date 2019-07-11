ced86aa144e269b34808a9164314c3219ae97bea
ninjawriter222 <devin.a.poe@gmail.com>
2019-07-11 10:02:44 -0700

    Add files via upload

--- assignments/lesson-10/scripts/eventpreston.js
+++ assignments/lesson-10/scripts/eventpreston.js
@@ -1,12 +1,26 @@
 let eventRequest = new XMLHttpRequest();
 let eventjson = 'https://byui-cit230.github.io/weather/data/towndata.json'
 eventRequest.open('Get', eventjson, true);
-eventRequest.responseType = 'json';
 eventRequest.send();
 
+eventRequest.onload=function(){
+    let eventData = JSON.parse(eventRequest.responseText);
+    console.log(eventData);
 
-eventRequest.onload = function() {
-    var town = eventRequest.response;
+    document.getElementById('motto').innerHTML = eventData.towns[4].motto;
+    document.getElementById('townname').innerHTML = eventData.towns[4].name;
+    document.getElementById('founded').innerHTML = eventData.towns[4].yearFounded;
+}
+
+let new1 = document.querySelector('new');
+let eventjson1 = 'https://byui-cit230.github.io/weather/data/towndata.json'
+let eventRequest1 = new XMLHttpRequest();
+eventRequest1.open('Get', eventjson1);
+eventRequest1.responseType = 'json';
+eventRequest1.send();
+
+eventRequest1.onload = function() {
+    var town = eventRequest1.response;
     towns(town);
 }
 
